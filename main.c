d3a0a8b00862ff4fa356818de9616737279dde70
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2024-11-17 21:56:59 -0300

    tentativa1 main.c

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+    int tam_vetor;
+    scanf("%d", &tam_vetor);
+    
+    int* vetor = (int*) malloc(tam_vetor*sizeof(int));
+    
+    for(int i = 0; i < tam_vetor; i++){
+        
+        scanf("%d", &vetor[i]);
+    }
+    
+    for(int i = 0; i < tam_vetor; i++){
+        
+        printf("%d ", vetor[i]);
+    }
+    
+    free(vetor);
+    
     return 0;
 }
